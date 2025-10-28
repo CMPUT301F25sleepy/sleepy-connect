0d1013fbb474b6325684370c3fdca0bf54c90e72
LightTerranova <dudiy@ualberta.ca>
2025-10-28 13:05:07 -0600

    Creating the DAL. - mb for working on main

---
+++ app/src/main/java/com/example/sleepy_connect/DAL.java
@@ -0,0 +1,7 @@
+package com.example.sleepy_connect;
+
+import androidx.constraintlayout.helper.widget.Layer;
+
+public class DAL {
+    /*Data Access Layer - Stores all functions that interface with the database.*/
+}
