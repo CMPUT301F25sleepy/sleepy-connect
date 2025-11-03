ba44fca9d4424df863db5bd6ed075669512bdc47
Samuel Francisco <dcsamfrancisco@gmail.com>
2025-11-02 22:56:55 -0700

    Partially implement CreateEventFragment

--- app/src/main/java/com/example/sleepy_connect/eventdetails/CreateEventFragment.java
+++ app/src/main/java/com/example/sleepy_connect/eventdetails/CreateEventFragment.java
@@ -2,48 +2,52 @@ package com.example.sleepy_connect.eventdetails;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.sleepy_connect.R;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link CreateEventFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Fragment class for creating new events
  */
 public class CreateEventFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String UID = "uid"; // to be updated
+    private String uid;
+    private boolean imageSet = false;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText title;
+    EditText reg_start;
+    EditText reg_end;
+    EditText event_start;
+    EditText capacity;
+    EditText description;
 
     public CreateEventFragment() {
         // Required empty public constructor
     }
 
     /**
-     * Use this factory method to create a new instance of
+     * Factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param uid Current user's id.
      * @return A new instance of fragment CreateEventFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static CreateEventFragment newInstance(String param1, String param2) {
+    public static CreateEventFragment newInstance(String uid) {
         CreateEventFragment fragment = new CreateEventFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(UID, uid);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,9 +55,10 @@ public class CreateEventFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // retrieve parameters
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            uid = getArguments().getString(UID);
         }
     }
 
@@ -62,5 +67,46 @@ public class CreateEventFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit_event, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // link views
+
+        ImageView poster = view.findViewById(R.id.edit_event_poster);
+        SwitchCompat geolocation = view.findViewById(R.id.edit_geolocation_switch);
+        Button save = view.findViewById(R.id.event_confirm_edit_button);
+
+        title = view.findViewById(R.id.edit_event_title);
+        reg_start = view.findViewById(R.id.edit_reg_start_date);
+        reg_end = view.findViewById(R.id.edit_reg_end_date);
+        event_start = view.findViewById(R.id.edit_event_start_date);
+        capacity = view.findViewById(R.id.edit_capacity_value);
+        description = view.findViewById(R.id.edit_event_descr_text);
+
+        // set listeners
+        poster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        geolocation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
