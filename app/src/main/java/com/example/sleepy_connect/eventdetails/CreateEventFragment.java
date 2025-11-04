77f40578aeb867ff7e9e8f4121b3c4d0503f60c0
Samuel Francisco <dcsamfrancisco@gmail.com>
2025-11-04 00:13:49 -0700

    change onclicklisteners to lambda functions

--- app/src/main/java/com/example/sleepy_connect/eventdetails/CreateEventFragment.java
+++ app/src/main/java/com/example/sleepy_connect/eventdetails/CreateEventFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,24 +15,20 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.sleepy_connect.R;
 
 /**
  * Fragment class for creating new events
+ * @author Sam Francisco
  */
 public class CreateEventFragment extends Fragment {
 
     private static final String UID = "uid"; // to be updated
     private String uid;
     private boolean imageSet = false;
-
-    EditText title;
-    EditText reg_start;
-    EditText reg_end;
-    EditText event_start;
-    EditText capacity;
-    EditText description;
+    private boolean geolocationOn = false;
 
     public CreateEventFragment() {
         // Required empty public constructor
@@ -73,40 +70,86 @@ public class CreateEventFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // link views
+        // initiate view listeners
+
+        ConstraintLayout regStart = view.findViewById(R.id.edit_reg_start_button);
+        regStart.setOnClickListener(v -> updateDateTime(v, R.id.edit_reg_start_date, R.id.edit_reg_start_time));
+
+        ConstraintLayout regEnd = view.findViewById(R.id.edit_reg_end_button);
+        regEnd.setOnClickListener(v -> updateDateTime(v, R.id.edit_reg_end_date, R.id.edit_reg_end_time));
+
+        ConstraintLayout eventStart = view.findViewById(R.id.edit_event_start_button);
+        eventStart.setOnClickListener(v -> updateDateTime(v, R.id.edit_event_start_date, R.id.edit_event_start_time));
+
+        ConstraintLayout eventEnd = view.findViewById(R.id.edit_event_end_button);
+        eventEnd.setOnClickListener(v -> updateDateTime(v, R.id.edit_event_end_date, R.id.edit_event_end_time));
 
         ImageView poster = view.findViewById(R.id.edit_event_poster);
-        SwitchCompat geolocation = view.findViewById(R.id.edit_geolocation_switch);
-        Button save = view.findViewById(R.id.event_confirm_edit_button);
-
-        title = view.findViewById(R.id.edit_event_title);
-        reg_start = view.findViewById(R.id.edit_reg_start_date);
-        reg_end = view.findViewById(R.id.edit_reg_end_date);
-        event_start = view.findViewById(R.id.edit_event_start_date);
-        capacity = view.findViewById(R.id.edit_capacity_value);
-        description = view.findViewById(R.id.edit_event_descr_text);
-
-        // set listeners
         poster.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-            }
-        });
+                // TODO: get image
 
-        geolocation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                // TODO: set imageSet if successful
 
             }
         });
 
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
+        SwitchCompat geolocationSwitch = view.findViewById(R.id.edit_geolocation_switch);
+        geolocationSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // record state when changed
+            geolocationOn = isChecked;
         });
 
+        Button saveBtn = view.findViewById(R.id.event_confirm_edit_button);
+        saveBtn.setOnClickListener(v -> {
+
+            // link views
+            EditText title = view.findViewById(R.id.edit_event_title);
+            EditText eventCapacity = view.findViewById(R.id.edit_event_capacity_value);
+            EditText waitlistCapacity = view.findViewById(R.id.edit_waitlist_capacity_value);
+            EditText recCenter = view.findViewById(R.id.edit_host_rec_center_text);
+            EditText address = view.findViewById(R.id.edit_host_address_text);
+            EditText description = view.findViewById(R.id.edit_event_descr_text);
+            TextView regStartDate = view.findViewById(R.id.edit_reg_start_date);
+            TextView regStartTime = view.findViewById(R.id.edit_reg_start_time);
+            TextView regEndDate = view.findViewById(R.id.edit_reg_end_date);
+            TextView regEndTime = view.findViewById(R.id.edit_reg_end_time);
+            TextView eventStartDate = view.findViewById(R.id.edit_event_start_date);
+            TextView eventStartTime = view.findViewById(R.id.edit_event_start_time);
+            TextView eventEndDate = view.findViewById(R.id.edit_event_end_date);
+            TextView eventEndTime = view.findViewById(R.id.edit_event_end_time);
+
+            // TODO: check if mandatory fields are set
+
+            // TODO: unset mandatory fields -> highlight unset fields, click done
+
+            // TODO: store data in database, click done
+
+        });
+    }
+
+    public void updateDateTime(View view, int dateViewId, int timeViewId) {
+
+        // TODO: open date time picker activity and get date and time
+
+        // TODO: update event_end date and time
+        TextView dateView = view.findViewById(dateViewId);
+        TextView timeView = view.findViewById(timeViewId);
+
+    }
+
+    public boolean mandatoryFieldsFilled(EditText title, EditText eventCapacity, EditText recCenter, EditText address,
+                                     TextView regStartDate, TextView regEndDate, TextView eventStartDate) {
+        // TODO: logical or all the views' values' isEmpty
+        return true;
+    }
+
+    public void highlightErrorFields(EditText title, EditText eventCapacity, EditText recCenter, EditText address,
+                                     TextView regStartDate, TextView regEndDate, TextView eventStartDate) {
+        // TODO: change all empty mandatory views' borders to red
+
+        // TODO: show error message
     }
 }
