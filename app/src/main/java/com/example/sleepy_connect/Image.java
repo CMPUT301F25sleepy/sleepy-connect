6fffc1078049f0b1b5eb38d041232e0c0cf42b1d
LightTerranova <dudiy@ualberta.ca>
2025-11-06 17:58:39 -0700

    adding boilerplate for image

--- app/src/main/java/com/example/sleepy_connect/Image.java
+++ app/src/main/java/com/example/sleepy_connect/Image.java
@@ -1,4 +1,12 @@
 package com.example.sleepy_connect;
 
+import android.net.Uri;
+
 public class Image {
+
+    private Uri imageUri;
+
+    public Image (Uri uri) {
+        this.imageUri = uri;
+    }
 }
