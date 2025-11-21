140188fd19a6e0078b788793339eddce809eddd7
Samuel Francisco <dcsamfrancisco@gmail.com>
2025-11-20 18:39:59 -0700

    get current user's uid passed with intent

--- app/src/main/java/com/example/sleepy_connect/admin/AdminActivity.java
+++ app/src/main/java/com/example/sleepy_connect/admin/AdminActivity.java
@@ -1,25 +1,26 @@
 package com.example.sleepy_connect.admin;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.sleepy_connect.R;
 import com.example.sleepy_connect.admin.profilemanagement.AdminProfileListFragment;
 
 public class AdminActivity extends AppCompatActivity {
 
+    public String currentUID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin);
+
+        // retrieve current user data
+        currentUID = getIntent().getStringExtra("uid");
 
         // set admin profile list as default child fragment
         getSupportFragmentManager()
